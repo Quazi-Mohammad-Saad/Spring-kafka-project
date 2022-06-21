1e4a5c18ece7d2a832879d7c552f1e4caab6a41d
Quazi <quazi1996saad@gmail.com>
2022-06-21 16:49:09 +0530

    Initial commit: Creating topic and Producer

---
+++ src/test/java/quazi/example/springkafkaproject/SpringKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package quazi.example.springkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
